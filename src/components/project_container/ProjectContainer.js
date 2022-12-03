b978f5dc8d607e4f127914bce51ca617efdadc89
Melissa Anastasia Harijanto <97304787+melissaharijanto@users.noreply.github.com>
2022-12-03 23:27:25 +0700

    Modify project container
    * Add size customization for various devices
    * Add components for description, contributions  and duration of project

--- src/components/project_container/ProjectContainer.js
+++ src/components/project_container/ProjectContainer.js
@@ -4,7 +4,7 @@ import WebsiteButton from "./WebsiteButton";
 import Github from '../../images/github.png';
 import { vmin } from '../../functions/UnitConverter.js'
 
-const ProjectContainer = ({projectName, teamName, iconSource, techStack, getLink, webLink, repoLink, contributions}) => {
+const ProjectContainer = ({showHome, projectName, teamName, iconSource, techStack, getLink, webLink, repoLink, description, duration, contributions}) => {
 
     const [width, setWindowWidth] = useState(window.innerWidth);
 
@@ -20,7 +20,6 @@ const ProjectContainer = ({projectName, teamName, iconSource, techStack, getLink
     const updateDimensions = () => {
       const width = window.innerWidth;
       setWindowWidth(width);
-
     }
 
     const stylePicker = () => {
@@ -35,13 +34,32 @@ const ProjectContainer = ({projectName, teamName, iconSource, techStack, getLink
         }
     }
 
+    const fontSizeHeadingPicker = () =>{
+        if (width > 0 && width <= 600) {
+            return "mobile-paragraph-heading"
+        } else {
+            return "paragraph-heading";
+        }
+    }
+
+
+    const fontSizeContentPicker = () =>{
+        if (width > 0 && width <= 600) {
+            return "mobile-paragraph-content"
+        } else if (width > 600 && width <= 1550){
+            return "paragraph-content-2";
+        } else {
+            return "paragraph-content";
+        }
+    }
+
     let techStackList = "";
 
     const listTechStack = () => {
         if (techStack != null) {
             for (let i = 0; i < techStack.length; i++) {
                 techStackList += techStack[i];
-                if (i != techStack.length - 1) {
+                if (i !== techStack.length - 1) {
                     techStackList += " " + String.fromCharCode("0x00B7") + " ";
                 }
             }
@@ -60,8 +78,14 @@ const ProjectContainer = ({projectName, teamName, iconSource, techStack, getLink
 
     return (
         <div className={stylePicker()}>
+            <div className='vertical-display-left-align'>
+                <button onClick={showHome} className="transparent-button">
+                    <text className="back-button">&#5176; Back</text>
+                </button>
+            </div>
+            <hr/>
             <div className="horizontal-display">
-                <img src={iconSource} className="app-icon"/>
+                <img src={iconSource} className="app-icon" alt={projectName}/>
                 <div className="vertical-display-left-align app-left-spacing">
                     <text className="app-heading">{projectName}</text>
                     <text className="app-subheading">{teamName}</text>
@@ -69,7 +93,7 @@ const ProjectContainer = ({projectName, teamName, iconSource, techStack, getLink
                         <GetButton link={getLink}/>
                         <WebsiteButton link={webLink}/>
                         <button className="repo-button" onClick={openRepo(repoLink)}>
-                            <img src={Github} width={getButtonHeight()} height={getButtonHeight()} className="git-repo-img"/>
+                            <img src={Github} width={getButtonHeight()} height={getButtonHeight()} alt="Project Repo Link" className="git-repo-img"/>
                         </button>
                     </div>
                     
@@ -79,8 +103,14 @@ const ProjectContainer = ({projectName, teamName, iconSource, techStack, getLink
                 </div>
             </div>
             <hr></hr>
-            <p>Description</p>
-            <p>{contributions}</p>
+            <div className='app-body vertical-display-left-align'>
+                <text className={fontSizeHeadingPicker()}>Description</text>
+                <text className={fontSizeContentPicker()}>{description}</text>
+                <text className={fontSizeHeadingPicker()}>Duration</text>
+                <text className={fontSizeContentPicker()}>{duration}</text>
+                <text className={fontSizeHeadingPicker()}>Contributions</text>
+                {contributions}
+            </div>
         </div>
     )
 }
