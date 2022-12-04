35fc7fb8772e216c9d9d64324cf5dd61a14d09c6
Melissa Anastasia Harijanto <97304787+melissaharijanto@users.noreply.github.com>
2022-12-05 00:14:50 +0700

    Remove contact link and replace with home

--- src/components/NavigationBar.js
+++ src/components/NavigationBar.js
@@ -7,9 +7,9 @@ const NavigationBar = () => {
                 <img src={Logo} alt="melissaharijanto" className='logo'/>
             </a>
             <div className='navigation-bar'>
+                <a href="#intro" className='navigation-link'>home</a>
                 <a href="#about" className='navigation-link'>about</a>
                 <a href="#projects" className='navigation-link'>projects</a>
-                <a href="#placeholder" className='navigation-link'>contact</a>
                 <a href="https://drive.google.com/file/d/1L8POkttcbFa-k3ISztDwkUAnVZtyVIe6/view?usp=sharing" className='navigation-link'>resume</a>
             </div>
         </div>
