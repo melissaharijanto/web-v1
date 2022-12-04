2c9d3a398905a7bf499aedbc647f71c9b249c94b
Melissa Anastasia Harijanto <97304787+melissaharijanto@users.noreply.github.com>
2022-12-05 00:59:30 +0700

    Add shadows and footer

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import NavigationBar from './components/NavigationBar';
 import About from './pages/About';
 import Intro from './pages/Intro';
 import Projects from './pages/Projects';
+import Footer from './pages/Footer';
 
 function App() {
 
@@ -13,6 +14,7 @@ function App() {
         <Intro/>
         <About/>
         <Projects/>
+        <Footer/>
       </div>
       
     </div>
---
+++ src/pages/Footer.js
@@ -0,0 +1,13 @@
+const Footer = () => {
+    return (
+        <div className="footer">
+            <div className="footer-text-container">
+                <text>Designed and built by Melissa Anastasia Harijanto with ReactJS, HTML, and CSS.</text>
+                <br/>
+                <text>Contact me <a href="mailto:melissaharijanto18@gmail.com" className="dark-link-color">here</a> (email)!</text>
+            </div>
+        </div>
+    )
+}
+
+export default Footer;
