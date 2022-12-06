b3caf5951139cffbf19d52e9c39da6ef1a6cde78
Melissa Anastasia Harijanto <97304787+melissaharijanto@users.noreply.github.com>
2022-12-06 12:39:12 +0700

    Fix scrolling problem upon load

--- src/App.js
+++ src/App.js
@@ -4,9 +4,14 @@ import About from './pages/About';
 import Intro from './pages/Intro';
 import Projects from './pages/Projects';
 import Footer from './pages/Footer';
+import { useEffect } from 'react';
 
 function App() {
 
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [])
+  
   return (
     <div className="App">
       <NavigationBar/>  
